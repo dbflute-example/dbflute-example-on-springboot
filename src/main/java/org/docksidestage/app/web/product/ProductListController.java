ad46755d15a36b762b9669864b85db793742fac6
shin-kinoshita <kinoshita116@gmail.com>
2019-07-03 03:15:37 +0900

    remove bug of product list page

--- src/main/java/org/docksidestage/app/web/product/ProductListController.java
+++ src/main/java/org/docksidestage/app/web/product/ProductListController.java
@@ -1,10 +1,12 @@
 package org.docksidestage.app.web.product;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.dbflute.cbean.result.PagingResultBean;
+import org.docksidestage.app.utils.ExampleStringUtils;
 import org.docksidestage.dbflute.allcommon.CDef;
 import org.docksidestage.dbflute.exbhv.ProductBhv;
 import org.docksidestage.dbflute.exentity.Product;
@@ -39,6 +41,7 @@ public class ProductListController {
     public String index(Model model, @PathVariable("pageNumber") Optional<Integer> pageNumber,
             @ModelAttribute(name = "searchForm") @Validated ProductSearchForm searchForm, BindingResult br) {
         if (br.hasErrors()) {
+            model.addAttribute("beans", Collections.emptyList());
             return "product/product_list";
         }
         PagingResultBean<Product> page = selectProductPage(pageNumber.orElse(1), searchForm);
@@ -60,15 +63,15 @@ public class ProductListController {
             cb.specify().derivedPurchase().max(purchaseCB -> {
                 purchaseCB.specify().columnPurchaseDatetime();
             }, Product.ALIAS_latestPurchaseDate);
-            if (existsField(form.getProductName())) {
+            if (ExampleStringUtils.isNotEmpty(form.getProductName())) {
                 cb.query().setProductName_LikeSearch(form.getProductName(), op -> op.likeContain());
             }
-            if (existsField(form.getPurchaseMemberName())) {
+            if (ExampleStringUtils.isNotEmpty(form.getPurchaseMemberName())) {
                 cb.query().existsPurchase(purchaseCB -> {
                     purchaseCB.query().queryMember().setMemberName_LikeSearch(form.getPurchaseMemberName(), op -> op.likeContain());
                 });
             }
-            if (existsField(form.getProductStatus())) {
+            if (ExampleStringUtils.isNotEmpty(form.getProductStatus())) {
                 cb.query().setProductStatusCode_Equal_AsProductStatus(CDef.ProductStatus.codeOf(form.getProductStatus()));
             }
             cb.query().addOrderBy_ProductName_Asc();
@@ -93,9 +96,5 @@ public class ProductListController {
         bean.setRegularPrice(product.getRegularPrice());
         bean.setLatestPurchaseDate(product.getLatestPurchaseDate());
         return bean;
-    }
-
-    private boolean existsField(String str) {
-        return str != null && str.length() > 0;
     }
 }
