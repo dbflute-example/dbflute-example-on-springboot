90f80d056107d8ae172dd7f3cd6e02cec9e873c1
shin-kinoshita <kinoshita116@gmail.com>
2019-07-03 02:58:55 +0900

    add validator for signup page

--- src/main/java/org/docksidestage/app/web/signup/SignupController.java
+++ src/main/java/org/docksidestage/app/web/signup/SignupController.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,6 +35,13 @@ public class SignupController {
     private MemberServiceBhv memberServiceBhv;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private SignupValidator signupValidator;
+
+    @InitBinder("signupForm")
+    public void initBinder(WebDataBinder binder) {
+        binder.addValidators(signupValidator);
+    }
 
     @GetMapping
     public String indexGet(Model model) {
