6e615444ea549ada5dc09b3dccc3b43e2e6aced4
Shimizu, Yuko <shimizu.y.af@gmail.com>
2019-07-15 22:36:12 +0900

    改行を他と合わせる

--- src/main/java/org/docksidestage/app/application/security/SecurityConfig.java
+++ src/main/java/org/docksidestage/app/application/security/SecurityConfig.java
@@ -28,9 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // #for_now for test, enable later by jflute
         http.authorizeRequests()
                 .antMatchers("/signin", "/signin/", "/signup", "/signup/", "/product/list", "/product/list/")
-                .permitAll()
-                .antMatchers("/css/**", "/js/**", "/images/**")
-                .permitAll().antMatchers("/lido/product/list").permitAll()
+                .permitAll().antMatchers("/css/**", "/js/**", "/images/**").permitAll().antMatchers("/lido/product/list").permitAll()
                 .anyRequest()
                 .authenticated();
 
