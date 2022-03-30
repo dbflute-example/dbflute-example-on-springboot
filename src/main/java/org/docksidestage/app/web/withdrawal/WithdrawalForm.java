58774fd5827b4617d67e6537baeffa45818f34cf
Shimizu, Yuko <shimizu.y.af@gmail.com>
2022-03-30 22:32:31 +0900

    Withdrwal 画面遷移を作成
    - Controller, Formを作成し、画面初期表示と遷移を実装
    - entry.htmlをリネーム。他に併せて機能名のprefix withdrawal を付ける
    - withdrawal_confirm.htmlを作成し、退会確認を実装

--- src/main/java/org/docksidestage/app/web/withdrawal/WithdrawalForm.java
+++ src/main/java/org/docksidestage/app/web/withdrawal/WithdrawalForm.java
@@ -1,4 +1,27 @@
 package org.docksidestage.app.web.withdrawal;
-public class WithdrawalForm {
 
+/**
+ * @author y.shimizu
+ */
+public class WithdrawalForm {
+    /** 退会理由コード */
+    private String reasonCode;
+    /** 退会理由 */
+    private String reasonInput;
+
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    public String getReasonCode() {
+        return reasonCode;
+    }
+    public void setReasonCode(String reasonCode) {
+        this.reasonCode = reasonCode;
+    }
+    public String getReasonInput() {
+        return reasonInput;
+    }
+    public void setReasonInput(String reasonInput) {
+        this.reasonInput = reasonInput;
+    }
 }
