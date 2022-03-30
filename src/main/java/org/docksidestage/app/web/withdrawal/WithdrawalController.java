fed1ee1224c643b4ed6f28845f17192c063b24df
Shimizu, Yuko <shimizu.y.af@gmail.com>
2022-03-30 22:32:31 +0900

    Withdrwal 画面遷移を作成
    - Controller, Formを作成し、画面初期表示と遷移を実装
    - entry.htmlをリネーム。他に併せて機能名のprefix withdrawal を付ける
    - withdrawal_confirm.htmlを作成し、退会確認を実装

--- src/main/java/org/docksidestage/app/web/withdrawal/WithdrawalController.java
+++ src/main/java/org/docksidestage/app/web/withdrawal/WithdrawalController.java
@@ -1,9 +1,13 @@
 package org.docksidestage.app.web.withdrawal;
 
+import java.util.Optional;
+
+import org.dbflute.optional.OptionalThing;
 import org.docksidestage.dbflute.allcommon.CDef;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,10 +21,27 @@ public class WithdrawalController {
     //                                                                     Request Mapping
     //                                                                     ===============
     @GetMapping("/")
-    public String showEntry(Model model, WithdrawalForm form) {
+    public String showEntry(Model model) {
+        // 表示する退会区分は区分値から取得する。
+        // 区分値はデータベース上管理されたコードなので、画面表示用には本来専用のコードに詰め替えた方がいいのかもしれない
         model.addAttribute("withdrawalReasons", CDef.WithdrawalReason.values());
-        model.addAttribute("errors"); // TODO
 
-        return "withdrawal/entry";
+        return "withdrawal/withdrawal_entry";
+    }
+
+    @PostMapping("/confirm")
+    public String showConfirm(Model model, WithdrawalForm form) {
+        model.addAttribute("selectedReason", form.getReasonCode());
+        model.addAttribute("selectedReasonText", CDef.WithdrawalReason.of(form.getReasonCode()).orElseThrow().alias());
+        model.addAttribute("reasonInput", form.getReasonInput());
+
+        return "withdrawal/withdrawal_confirm";
+
+    }
+
+    @PostMapping("/done")
+    public String done(WithdrawalForm form) {
+        // TODO 退会処理
+        return "redirect:/signin";
     }
 }
