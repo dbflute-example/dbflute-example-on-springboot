01e3a3195236a8964a48ce54807a65ad9d8e8f16
jflute <jflute@localhost>
2022-05-14 12:10:56 +0900

    update _project.sh, new JAVA_HOME process

--- dbflute_maihamadb/_project.sh
+++ dbflute_maihamadb/_project.sh
@@ -2,10 +2,15 @@
 
 export ANT_OPTS=-Xmx512m
 
-export DBFLUTE_HOME=../mydbflute/dbflute-1.2.6
+export DBFLUTE_HOME=../mydbflute/dbflute-1.x
 
 export MY_PROPERTIES_PATH=build.properties
 
-if [ `uname` = "Darwin" ]; then
-  export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+JAVA_VERSION=1.8+
+
+# auto-setting for JAVA_HOME (keeping exiting JAVA_HOME)
+if [[ -z "${JAVA_HOME}" ]]; then
+  if [[ `uname` = "Darwin" ]]; then
+    export JAVA_HOME=$(/usr/libexec/java_home -v ${JAVA_VERSION})
+  fi
 fi
